08c1f2618d0586474bfba9afe03e40ce1339343a
caroline <caroline.hjq@gmail.com>
2016-01-11 22:21:33 +0800

    Add some folders to manage project better

---
+++ CASeekForCity/Classes/Main/Controller/CAMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAMainViewController.h
+//  CASeekForCity
+//
+//  Created by Caroline.H on 1/9/16.
+//  Copyright © 2016 Caroline.H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAMainViewController : UIViewController
+
+@end
---
+++ CASeekForCity/Classes/Main/Controller/CAMainViewController.m
@@ -0,0 +1,13 @@
+//
+//  CAMainViewController.m
+//  CASeekForCity
+//
+//  Created by Caroline.H on 1/9/16.
+//  Copyright © 2016 Caroline.H. All rights reserved.
+//
+
+#import "CAMainViewController.h"
+
+@implementation CAMainViewController
+
+@end
